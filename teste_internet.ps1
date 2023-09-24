631ce58f286d408ac2c8247927c3d8df464f8899
Helio-Inocencio <66126579+Helio-Inocencio@users.noreply.github.com>
2023-09-24 13:38:45 -0300

    Delete teste_internet.ps1

--- teste_internet.ps1
+++
@@ -1,8 +0,0 @@
-<#Teste de internet#>
-
-Clear-Host
-$conn = (Test-Connection -Ping www.google.com -Count 1 -Quiet)
-if ($conn -eq "true") {
-    <# Action to perform if the condition is true #>
-    Write-Host "Internet Funcionando"
-}
