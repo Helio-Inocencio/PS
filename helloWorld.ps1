45b4913ff9efefdeee26e4a63c9b0bdc2c980ec9
Helio-Inocencio <66126579+Helio-Inocencio@users.noreply.github.com>
2023-09-24 13:39:16 -0300

    Delete helloWorld.ps1

--- helloWorld.ps1
+++
@@ -1,7 +0,0 @@
-# Novo teste de script em powershell
-# variaveis
-clear-Host
-$nome= Read-host "Quel e o seu nome?"
-write-host "Ola $nome, bem vindo, voce esta bem?"
-write-host "Mais um teste de power shell feito com sucesso."
-
